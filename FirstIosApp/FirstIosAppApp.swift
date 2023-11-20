2ffc54da6c381e5bf7b9e0aa1b8554613a7cc22b
Joshua Biong <joshuabiong7@gmail.com>
2023-11-21 03:32:00 +0800

    Initial Commit

---
+++ FirstIosApp/FirstIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIosAppApp.swift
+//  FirstIosApp
+//
+//  Created by Joshua P. Biong on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
